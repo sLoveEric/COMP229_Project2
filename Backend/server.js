8fe8b4bda0669838a52737847ac9e2553e39a3d7
Sean Wang <s.wang.619@gmail.com>
2023-06-20 08:15:10 -0400

    modify server,js

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-//const db = require("./app/models");
+const db = require("./app/models");
 
-// db.mongoose
+db.mongoose
 
-//   .connect(db.url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
-//   .then(() => {
+  .then(() => {
 
-//     console.log("Connected to the database!");
+    console.log("Connected to the database!");
 
-//   })
+  })
 
-//   .catch(err => {
-//     console.log("Cannot connect to the database!", err);
-//     process.exit();
-//   });
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -38,8 +38,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Shop application." });
 });
 
-// require("./app/routes/product.routes")(app); 
-// require("./app/routes/category.routes")(app);
+require("./app/routes/product.routes")(app); 
+require("./app/routes/category.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8079;
